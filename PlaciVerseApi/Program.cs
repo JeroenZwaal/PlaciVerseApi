a7b49922d98b2f0e8b5a4a5ca76d72ee561052d6
Jeroen Zwaal <jeroen@zwaaltjes.nl>
2025-03-10 15:16:00 +0100

    changed program.cs

--- PlaciVerseApi/Program.cs
+++ PlaciVerseApi/Program.cs
@@ -40,10 +40,10 @@ namespace PlaciVerseApi
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            /*if (app.Environment.IsDevelopment())
             {
                 
-            }
+            }*/
 
             app.UseSwagger();
             app.UseSwaggerUI();
